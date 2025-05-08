95a7caa6a881c0a6cd5f7c69518f49b8363b4ce3
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-05-08 18:41:32 +0530

    Tuples Started

---
+++ Arrays/array.ts
@@ -0,0 +1,22 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let numbers2 = [1, 2, 3, 4, 5, 6, 7, "8", 9, 10];
+
+let num: number[] = [];
+let num2: (number | string)[] = [];
+
+num[0] = 1;
+num[1] = 2;
+
+num2[0] = 1;
+num2[1] = "2";
+num2[2] = 3;
+num2[3] = 4;
+
+num[2] = 3;
+
+
+console.log(numbers); // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+console.log(numbers2); // [ 1, 2, 3, 4, 5, 6, 7, '8', 9, 10 ]
+console.log(num); // [ 1, 2, 3 ]
+console.log(num2); // [ 1, '2', 3, 4 ]
---
+++ turples/turpes.ts
@@ -0,0 +1,5 @@
+let user:[number, string, boolean] = [1, "John", true];
+let user2:[number, string, boolean?] = [1, "John"];
+
+console.log(user); // [ 1, 'John', true ]
+console.log(user2); // [ 1, 'John' ]
